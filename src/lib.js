91a13cbcc709a6ff72a36170127da91e64bdd69a
Joshua Cilufo <joshua.cilufo@student.swic.edu>
2025-10-25 22:58:35 -0500

    feat: :sparkles: return winning line when calculating winner

--- src/lib.js
+++ src/lib.js
@@ -1,8 +1,15 @@
 import { WINNING_INDICES } from "./constants.js";
 
-export const calculateWinner = (board) =>
-  ["X", "O"].find((letter) =>
-    WINNING_INDICES.some((winningIndices) =>
-      winningIndices.every((winningIndex) => board[winningIndex] === letter)
-    )
-  ) || null;
+export const calculateWinner = (board) => {
+  for (const player of ["X", "O"]) {
+    for (const line of WINNING_INDICES) {
+      if (line.every((index) => board[index] === player)) {
+        return {
+          winner: player,
+          line,
+        };
+      }
+    }
+  }
+  return null;
+};
