8d11094bff48d78a58f7d4f86bed1fbb2c4245a2
Joshua Cilufo <joshua.cilufo@student.swic.edu>
2025-10-26 18:58:22 -0500

    test: :fire: remove impossible test situation

--- src/lib.test.js
+++ src/lib.test.js
@@ -101,10 +101,4 @@ describe("calculateWinner", () => {
       expect(calculateWinner(board)).toEqual({ winner: "O", line: [2, 4, 6] });
     });
   });
-
-  test("prioritizes X over O when both somehow win (edge case)", () => {
-    // This shouldn't happen in a real game, but tests the implementation
-    const board = ["X", "X", "X", "O", "O", "O", null, null, null];
-    expect(calculateWinner(board)).toEqual({ winner: "X", line: [0, 1, 2] });
-  });
 });
